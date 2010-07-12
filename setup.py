291a26c389d0d3d71b58c6e7edde4a8916e14bd7
Adam Lindsay <atl@alum.mit.edu>
2010-07-12 21:34:05 +0100

    Added basic setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='dulse',
+      version='0.1',
+      packages=['dulse'],
+      description='Dumb, but light & simple module for XML',
+      author='Adam T. Lindsay',
+      author_email='atl@comp.lancs.ac.uk',
+      url='http://github.com/atl/dulse',
+      )
